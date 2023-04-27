e21f35f5bdd4db04d9b84291bce9bffbd3bb0dc1
Musyimi <musyimi13@gmail.com>
2023-04-27 12:08:09 +0300

    added the view_body function and also sorted the election results

--- lib/election.ex
+++ lib/election.ex
@@ -13,4 +13,31 @@ defmodule Election do
       "Election for: #{election.name}\n"
     ]
   end
+
+  def view_body(election) do
+    election.candidates
+    |> sort_candidates_by_votes_desc()
+    |> candidates_to_string()
+    |> prepend_candidates_header()
+  end
+
+  defp prepend_candidates_header(candidates) do
+    [
+      "ID\tVOTES\tNAME\n",
+      "---------------------------\n"
+      | candidates
+    ]
+  end
+
+  defp candidates_to_string(candidates) do
+    candidates
+    |> Enum.map(fn %{id: id, name: name, votes: votes} ->
+      "#{id}\t#{votes}\t#{name}\n"
+    end)
+  end
+
+  defp sort_candidates_by_votes_desc(candidates)  do
+    candidates
+    |> Enum.sort(&(&1.votes >= &2.votes))
+  end
 end
